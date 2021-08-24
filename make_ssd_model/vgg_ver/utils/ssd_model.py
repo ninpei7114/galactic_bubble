10fff92d6da943101b29d543af42872fb4000d9d
Nakatani Shuyo <nakatani@labs.cybozu.co.jp>
2021-08-24 17:47:41 +0900

    simplify conf loss caluculation

--- make_ssd_model/vgg_ver/utils/ssd_model.py
+++ make_ssd_model/vgg_ver/utils/ssd_model.py
@@ -862,8 +862,10 @@ class MultiBoxLoss(nn.Module):
         # 損失の計算に使用するものを格納する変数を作成
         # conf_t_label：各DBoxに一番近い正解のBBoxのラベルを格納させる
         # loc_t:各DBoxに一番近い正解のBBoxの位置情報を格納させる
-        conf_t_label = torch.LongTensor(num_batch, num_dbox).to(self.device)
-        loc_t = torch.Tensor(num_batch, num_dbox, 4).to(self.device)
+        #conf_t_label = torch.LongTensor(num_batch, num_dbox).to(self.device)
+        #loc_t = torch.Tensor(num_batch, num_dbox, 4).to(self.device)
+        conf_t_label = torch.zeros((num_batch, num_dbox), dtype=torch.long).to(self.device)
+        loc_t = torch.zeros((num_batch, num_dbox, 4), dtype=torch.float32).to(self.device)
 
         # loc_tとconf_t_labelに、
         # DBoxと正解アノテーションtargetsをmatchさせた結果を上書きする
@@ -888,112 +890,21 @@ class MultiBoxLoss(nn.Module):
             match(self.jaccard_thresh, truths, dbox,
                   variance, labels, loc_t, conf_t_label, idx)
 
-        # ----------
-        # 位置の損失：loss_lを計算
-        # Smooth L1関数で損失を計算する。ただし、物体を発見したDBoxのオフセットのみを計算する
-        # ----------
-        # 物体を検出したBBoxを取り出すマスクを作成
         pos_mask = conf_t_label > 0  # torch.Size([num_batch, 8732])
 
-        # pos_maskをloc_dataのサイズに変形
-        pos_idx = pos_mask.unsqueeze(pos_mask.dim()).expand_as(loc_data)
+        log_conf = -F.log_softmax(conf_data, dim=2) # 負の対数尤度
+        loss_c_posi = log_conf[:,:,1][pos_mask] # ←このコードは多クラスに対応しない
 
-        # Positive DBoxのloc_dataと、教師データloc_tを取得
-        loc_p = loc_data[pos_idx].view(-1, 4)
-        loc_t = loc_t[pos_idx].view(-1, 4)
+        log_conf_nega = log_conf[:,:,0] # クラス0(背景)の信頼度
+        log_conf_nega[pos_mask] = 0
+        _, loss_idx = log_conf_nega.sort(1, descending=True)
+        _, idx_rank = loss_idx.sort(1) # 各ボックスの順位 (e.g. idx_rank[n] はボックス #n の順位)
+        neg_idx = idx_rank < pos_mask.sum(1)[:,None] * self.negpos_ratio
+        loss_c_nega = log_conf_nega[neg_idx]
+        loss_c_avg = torch.cat((loss_c_posi, loss_c_nega)).mean()
 
-        # 物体を発見したPositive DBoxのオフセット情報loc_tの損失（誤差）を計算
-        loss_l = F.smooth_l1_loss(loc_p, loc_t, reduction='sum')
+        loc_p = loc_data[pos_mask]
+        loc_t = loc_t[pos_mask]
+        loss_l_avg = F.smooth_l1_loss(loc_p, loc_t)
 
-        # ----------
-        # クラス予測の損失：loss_cを計算
-        # 交差エントロピー誤差関数で損失を計算する。ただし、背景クラスが正解であるDBoxが圧倒的に多いので、
-        # Hard Negative Miningを実施し、物体発見DBoxと背景クラスDBoxの比が1:3になるようにする。
-        # そこで背景クラスDBoxと予想したもののうち、損失が小さいものは、クラス予測の損失から除く
-        # ----------
-        batch_conf = conf_data.view(-1, num_classes)
-#         print(batch_conf)
-
-#         print(torch.argmax(batch_conf, dim=1))
-#         print(conf_t_label.view(-1))
-#         print(conf_t_label.view(-1).max())
-#         print(batch_conf.shape)
-#         print(conf_t_label.view(-1).shape)
-        # クラス予測の損失を関数を計算(reduction='none'にして、和をとらず、次元をつぶさない)
-        loss_c = F.cross_entropy(
-            batch_conf, conf_t_label.view(-1), reduction='none')
-
-        # -----------------
-        # これからNegative DBoxのうち、Hard Negative Miningで抽出するものを求めるマスクを作成します
-        # -----------------
-
-        # 物体発見したPositive DBoxの損失を0にする
-        # （注意）物体はlabelが1以上になっている。ラベル0は背景。
-        num_pos = pos_mask.long().sum(1, keepdim=True)  # ミニバッチごとの物体クラス予測の数
-        loss_c = loss_c.view(num_batch, -1)  # torch.Size([num_batch, 8732])
-        loss_c[pos_mask] = 0  # 物体を発見したDBoxは損失0とする
-
-        # Hard Negative Miningを実施する
-        # 各DBoxの損失の大きさloss_cの順位であるidx_rankを求める
-        _, loss_idx = loss_c.sort(1, descending=True)
-        _, idx_rank = loss_idx.sort(1)
-
-        # （注釈）
-        # 実装コードがかなり特殊で直感的ではないです。
-        # 上記2行は、要は各DBoxに対して、損失の大きさが何番目なのかの情報を
-        # 変数idx_rankとして高速に取得したいというコードです。
-        #
-        # DBOXの損失値の大きい方から降順に並べ、DBoxの降順のindexをloss_idxに格納。
-        # 損失の大きさloss_cの順位であるidx_rankを求める。
-        # ここで、
-        # 降順になった配列indexであるloss_idxを、0から8732まで昇順に並べ直すためには、
-        # 何番目のloss_idxのインデックスをとってきたら良いのかを示すのが、idx_rankである。
-        # 例えば、
-        # idx_rankの要素0番目 = idx_rank[0]を求めるには、loss_idxの値が0の要素、
-        # つまりloss_idx[?}=0 の、?は何番かを求めることになる。ここで、? = idx_rank[0]である。
-        # いま、loss_idx[?]=0の0は、元のloss_cの要素の0番目という意味である。
-        # つまり?は、元のloss_cの要素0番目は、降順に並び替えられたloss_idxの何番目ですか
-        # を求めていることになり、 結果、
-        # ? = idx_rank[0] はloss_cの要素0番目が、降順の何番目かを示すことになる。
-
-        # 背景のDBoxの数num_negを決める。HardNegative Miningにより、
-        # 物体発見のDBoxの数num_posの3倍（self.negpos_ratio倍）とする。
-        # ただし、万が一、DBoxの数を超える場合は、DBoxの数を上限とする
-        num_neg = torch.clamp(num_pos*self.negpos_ratio, max=num_dbox)
-
-        # idx_rankは各DBoxの損失の大きさが上から何番目なのかが入っている
-        # 背景のDBoxの数num_negよりも、順位が低い（すなわち損失が大きい）DBoxを取るマスク作成
-        # torch.Size([num_batch, 8732])
-        neg_mask = idx_rank < (num_neg).expand_as(idx_rank)
-
-        # -----------------
-        # （終了）これからNegative DBoxのうち、Hard Negative Miningで抽出するものを求めるマスクを作成します
-        # -----------------
-
-        # マスクの形を整形し、conf_dataに合わせる
-        # pos_idx_maskはPositive DBoxのconfを取り出すマスクです
-        # neg_idx_maskはHard Negative Miningで抽出したNegative DBoxのconfを取り出すマスクです
-        # pos_mask：torch.Size([num_batch, 8732])→pos_idx_mask：torch.Size([num_batch, 8732, 21])
-        pos_idx_mask = pos_mask.unsqueeze(2).expand_as(conf_data)
-        neg_idx_mask = neg_mask.unsqueeze(2).expand_as(conf_data)
-
-        # conf_dataからposとnegだけを取り出してconf_hnmにする。形はtorch.Size([num_pos+num_neg, 21])
-        conf_hnm = conf_data[(pos_idx_mask+neg_idx_mask).gt(0)
-                             ].view(-1, num_classes)
-        # （注釈）gtは greater than (>)の略称。これでmaskが1のindexを取り出す。
-        # pos_idx_mask+neg_idx_maskは足し算だが、indexへのmaskをまとめているだけである。
-        # つまり、posであろうがnegであろうが、マスクが1のものを足し算で一つのリストにし、それをgtで取得
-
-        # 同様に教師データであるconf_t_labelからposとnegだけを取り出してconf_t_label_hnmに
-        # 形はtorch.Size([pos+neg])になる
-        conf_t_label_hnm = conf_t_label[(pos_mask+neg_mask).gt(0)]
-
-        # confidenceの損失関数を計算（要素の合計=sumを求める）
-        loss_c = F.cross_entropy(conf_hnm, conf_t_label_hnm, reduction='sum')
-
-        # 物体を発見したBBoxの数N（全ミニバッチの合計）で損失を割り算
-        N = num_pos.sum()
-        loss_l /= N
-        loss_c /= N
-
-        return loss_l, loss_c
+        return loss_c_avg, loss_l_avg
