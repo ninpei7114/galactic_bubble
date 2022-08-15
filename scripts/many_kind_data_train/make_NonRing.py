b6f058c3bdf0387c828f3b295d80450cb6e16553
shimpei <ninpei7114@gmail.com>
2022-08-15 19:27:24 +0900

    Non_Ring scriptを加えた

--- scripts/many_kind_data_train/make_NonRing.py
+++ scripts/many_kind_data_train/make_NonRing.py
@@ -18,7 +18,6 @@ import collections
 import copy
 import argparse
 
-from torchvision import transforms
 import torch
 from torch.nn import functional as F
 from torch import nn
@@ -28,29 +27,28 @@ import NonRing_sub
 
 
 
-# def parse_args():
-#     parser = argparse.ArgumentParser(description='make data for deepcluster')
+def parse_args():
+    parser = argparse.ArgumentParser(description='make data for deepcluster')
 
-#     parser.add_argument('fits_path', metavar='DIR', help='path to dataset')
-#     # parser.add_argument('ring_sentei_path', metavar='DIR', help='path to ring setntei file')
-#     # parser.add_argument('mwp_catalogu_path', metavar='DIR', help='path to mwp catalogue')
-#     # # parser.add_argument('augmentation_num', metavar='DIR', help='the number of augmentation')
-#     # parser.add_argument('savedir_format', metavar='DIR', help='data save dir name format')
-#     # parser.add_argument('--augmentation_num_list', required=True, nargs="*", type=int, help='the number of augmentation')
-#     # parser.add_argument('--savedir', default='ring_to_circle_nan_fits', 
-#     #                     help='data save dir')
+    parser.add_argument('fits_path', metavar='DIR', help='path to dataset')
+    # parser.add_argument('ring_sentei_path', metavar='DIR', help='path to ring setntei file')
+    # parser.add_argument('mwp_catalogu_path', metavar='DIR', help='path to mwp catalogue')
+    # # parser.add_argument('augmentation_num', metavar='DIR', help='the number of augmentation')
+    # parser.add_argument('savedir_format', metavar='DIR', help='data save dir name format')
+    # parser.add_argument('--augmentation_num_list', required=True, nargs="*", type=int, help='the number of augmentation')
+    # parser.add_argument('--savedir', default='ring_to_circle_nan_fits', 
+    #                     help='data save dir')
 
-#     return parser.parse_args()
+    return parser.parse_args()
 
 
 
-def make_nonring(train_epoch):
+def main(args):
 
     val_l = ['spitzer_00900+0000_rgb','spitzer_03900+0000_rgb','spitzer_31200+0000_rgb','spitzer_34200+0000_rgb','spitzer_33900+0000_rgb',]
     val_l = sorted(val_l)
 
-    train_l = [
-        'spitzer_02100+0000_rgb','spitzer_04200+0000_rgb','spitzer_33300+0000_rgb','spitzer_35400+0000_rgb','spitzer_00300+0000_rgb',
+    train_l = ['spitzer_02100+0000_rgb','spitzer_04200+0000_rgb','spitzer_33300+0000_rgb','spitzer_35400+0000_rgb','spitzer_00300+0000_rgb',
         'spitzer_02400+0000_rgb','spitzer_04500+0000_rgb','spitzer_31500+0000_rgb','spitzer_33600+0000_rgb','spitzer_35700+0000_rgb',
         'spitzer_00600+0000_rgb','spitzer_02700+0000_rgb','spitzer_04800+0000_rgb','spitzer_29700+0000_rgb','spitzer_31800+0000_rgb',
         'spitzer_03000+0000_rgb','spitzer_05100+0000_rgb','spitzer_30000+0000_rgb','spitzer_32100+0000_rgb','spitzer_01200+0000_rgb',
@@ -79,15 +77,14 @@ def make_nonring(train_epoch):
     for mode in ['train', 'val']:
 
         if mode == 'train':
-            epoch = 100
+            epoch = 300
             ref_path_list = train_l
             choice_num = len(train_l)-1
-            one_fits = 200
         else:
             epoch = 30
             ref_path_list = val_l
             choice_num = len(val_l)-1
-            one_fits = 100
+
 
         no_nan_no_ring_list = []
         start = time.time()
@@ -111,7 +108,7 @@ def make_nonring(train_epoch):
             # GLON_LAT関数でGLON_new_min1, GLON_new_max1, GLAT_new_min1, GLAT_new_max1を出す
             GLON_new_min1, GLON_new_max1, GLAT_new_min1, GLAT_new_max1 = NonRing_sub.GLON_LAT(data, header, w)
             
-            for k in range(one_fits):
+            for k in range(30):
 
                 cut_data = NonRing_sub.no_nan_ring(data, GLON_new_min1, GLON_new_max1, GLAT_new_min1, GLAT_new_max1, hist_, hisy, range_, w)
         #         print(cut_data.shape)
@@ -121,7 +118,7 @@ def make_nonring(train_epoch):
                 res_data = pi[int(r_shape_y/4):int(r_shape_y*3/4), int(r_shape_x/4):int(r_shape_x*3/4)]
                 res_data = proceesing.normalize(res_data)
                 res_data = proceesing.resize(res_data, 300)
-
+    
                 no_nan_no_ring_list.append(res_data)
                 
                 
@@ -132,8 +129,8 @@ def make_nonring(train_epoch):
 
 
 
-# if __name__ == '__main__':
-#     args = parse_args()
-#     main(args)
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
 
 
