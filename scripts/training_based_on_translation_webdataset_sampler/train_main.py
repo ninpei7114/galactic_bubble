9667f4d55da1419c9d76c77afcc48e47c2910ce2
shimpei <ninpei7114@gmail.com>
2023-01-31 02:07:16 +0900

    augmentationのTrue, Falseのバリエーションを変更

--- scripts/training_based_on_translation_webdataset_sampler/train_main.py
+++ scripts/training_based_on_translation_webdataset_sampler/train_main.py
@@ -80,8 +80,8 @@ def main(args):
     }
 
     # 上下反転、回転、縮小、平行移動の4パターンの組み合わせでaugmentatio を作る。
-    flip_list = [False, True]
-    rotate_list = [False, True]
+    flip_list = [False] #[False, True]
+    rotate_list = [True] #[False, True]
     scale_list = [False]
     translation_list = [True]
 
