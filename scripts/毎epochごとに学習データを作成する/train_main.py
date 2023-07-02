ae14e9d78eb243dece5c82d6233bad6b852cc18a
shimpei <ninpei7114@gmail.com>
2023-07-02 23:34:17 +0900

    augmentationのパターン変更

--- scripts/毎epochごとに学習データを作成する/train_main.py
+++ scripts/毎epochごとに学習データを作成する/train_main.py
@@ -65,8 +65,8 @@ def main(args):
     }
 
     ## 上下反転、回転、縮小、平行移動の4パターンの組み合わせでaugmentationをする。
-    flip_list = [True, False]
-    rotate_list = [True, False]
+    flip_list = [True]
+    rotate_list = [True]
     scale_list = [False]
     translation_list = [True]
 
