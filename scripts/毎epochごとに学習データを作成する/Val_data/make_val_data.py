da285456901492f931f8ea4725202e485bb29337
shimpei <ninpei7114@gmail.com>
2023-07-26 07:40:49 +0900

    Valid Dataの作り方を変更

--- scripts/毎epochごとに学習データを作成する/Val_data/make_val_data.py
+++ scripts/毎epochごとに学習データを作成する/Val_data/make_val_data.py
@@ -10,13 +10,12 @@ import numpy as np
 import pandas as pd
 from numpy.random import default_rng
 from PIL import Image
-from torch.nn import functional as F
 from tqdm import tqdm
 
 sys.path.append("../")
 import processing
-import val_label_calculator
-import val_ring_sub
+import label_caliculator
+import ring_augmentation
 
 """
 
@@ -32,6 +31,12 @@ def parse_args():
     parser.add_argument("--each_region", "-r", action="store_true")
 
     return parser.parse_args()
+
+
+def append_data(data, info, data_list, frame):
+    if not np.isnan(data.sum()):
+        data_list.append(data)
+        frame.append(info)
 
 
 def main(args):
@@ -73,13 +78,10 @@ def main(args):
         ]
         val_l = sorted(val_l)
 
-    # choice catalogue from 'CH' or 'MWP'
+    ## choice catalogue from 'CH' or 'MWP'
     choice = "CH"
-    Ring_CATA = val_ring_sub.catalogue(choice)
-
-    random_uni = default_rng(123)
-    sig1 = 1 / (2 * (np.log(2)) ** (1 / 2))
-
+    Ring_CATALOGUE = ring_augmentation.catalogue(choice)
+    trans_rng = default_rng(123)
     frame_mwp_val = []
     mwp_ring_list_val = []
     val_count = 0
@@ -90,9 +92,9 @@ def main(args):
 
     pbar = tqdm(range(len(val_l)))
     for i in pbar:
-        if args.each_region:
-            region_frame_mwp_val = []
-            region_mwp_ring_list_val = []
+        # if args.each_region:
+        #     region_frame_mwp_val = []
+        #     region_mwp_ring_list_val = []
 
         fits_path = val_l[i]
         pbar.set_description(fits_path)
@@ -121,168 +123,54 @@ def main(args):
         GLON_new_min = GLON_center - 1.5
         GLON_new_max = GLON_center + 1.5
 
-        Ring_cata = Ring_CATA.query("@GLON_new_min < GLON <= @GLON_new_max")
-        Ring_cata = Ring_cata.reset_index()
-        val_count += len(Ring_cata)
+        Ring_catalogue = Ring_CATALOGUE.query("@GLON_new_min < GLON <= @GLON_new_max")
+        val_count += len(Ring_catalogue)
 
         # star_listは辞書
-        label_cal = val_label_calculator.label_caliculator(choice, "val", w)
-        star_dic = label_cal.all_star(Ring_cata)
+        label_cal = label_caliculator.label_caliculator(choice, w)
+        label_cal.all_star(Ring_catalogue)
 
-        if choice == "CH":
-            Rout = "Rout"
-        else:
-            Rout = "Reff"
-
-        for _, row in Ring_cata.iterrows():
-            ccc = 0
-            ok = True
-
-            while ok:
-                random_num = 1 / random_uni.uniform(0.125, 0.8)
-                lmax = row["GLON"] + random_num * row[Rout] / 60
-                bmin = row["GLAT"] - random_num * row[Rout] / 60
-                # 右端
-                lmin = row["GLON"] - random_num * row[Rout] / 60
-                bmax = row["GLAT"] + random_num * row[Rout] / 60
-                ccc += 1
-                if GLON_min <= lmin and lmax <= GLON_max and GLAT_min <= bmin and bmax <= GLAT_max:
-                    ok = False
-                    flag = True
-                if ccc >= 100:
-                    ok = False
-                    flag = False
-            if flag:
-                x_min, y_min = w.all_world2pix(lmax, bmin, 0)
-                x_max, y_max = w.all_world2pix(lmin, bmax, 0)
-                r = int((x_max - x_min) / (2 * random_num))  # ringの半径pixel
-
-                width = x_max - x_min
-                height = y_max - y_min
-
-                x_pix_min = x_min - width / 2
-                y_pix_min = y_min - height / 2
-                x_pix_max = x_max + width / 2
-                y_pix_max = y_max + height / 2
-
-                x_offset = random_uni.uniform(-(random_num - 0.5) * r, (random_num - 0.5) * r)
-                y_offset = random_uni.uniform(-(random_num - 0.5) * r, (random_num - 0.5) * r)
-                x_pix_min = x_pix_min + int(x_offset)
-                x_pix_max = x_pix_max + int(x_offset)
-                y_pix_min = y_pix_min + int(y_offset)
-                y_pix_max = y_pix_max + int(y_offset)
-                width = x_pix_max - x_pix_min
-                height = y_pix_max - y_pix_min
-
-                # calc_pix時に100回試行してもできなかった場合の場合分け
-                if x_pix_min < 0 or y_pix_min < 0:
-                    pass
-                else:
-                    # print(x_pix_min, y_pix_min, x_pix_max, y_pix_max)
-                    cover_star_position, cover_star_name = label_cal.find_cover(
-                        star_dic, x_pix_min, y_pix_min, x_pix_max, y_pix_max
-                    )
-                    c_data = data[int(y_pix_min) : int(y_pix_max), int(x_pix_min) : int(x_pix_max)].view()
-                    cut_data = copy.deepcopy(c_data)
-                    if np.isnan(cut_data.sum()):
-                        pass
-
-                    else:
-                        sig1 = 1 / (2 * (np.log(2)) ** (1 / 2))
-                        pi = processing.conv(300, sig1, cut_data)
-                        label_cal.make_label(
-                            x_pix_min,
-                            y_pix_min,
-                            x_pix_max,
-                            y_pix_max,
-                            cover_star_position,
-                            cover_star_name,
-                            width,
-                            height,
-                            Ring_CATA,
-                        )
-                        r_shape_y = pi.shape[0]
-                        r_shape_x = pi.shape[1]
-                        res_data = pi[
-                            int(r_shape_y / 4) : int(r_shape_y * 3 / 4), int(r_shape_x / 4) : int(r_shape_x * 3 / 4)
-                        ]
-                        res_data = processing.norm_res(res_data)
-                        xmin_list, ymin_list, xmax_list, ymax_list, name_list = label_cal.check_list()
-                        info = {
-                            "fits": fits_path,
-                            "name": name_list,
-                            "xmin": xmin_list,
-                            "xmax": xmax_list,
-                            "ymin": ymin_list,
-                            "ymax": ymax_list,
-                        }
-
-                        #######################
-                        ## 完成したデータを格納 ###
-                        #######################
-                        if not np.isnan(res_data.sum()):
-                            if args.each_region:
-                                region_mwp_ring_list_val.append(res_data)
-                                region_frame_mwp_val.append(info)
-                            else:
-                                mwp_ring_list_val.append(res_data)
-                                frame_mwp_val.append(info)
+        for _, row in Ring_catalogue.iterrows():
+            trans_params = {
+                "row": row,
+                "fits_path": fits_path,
+                "GLON_new_min": GLON_new_min,
+                "GLON_new_max": GLON_new_max,
+                "GLAT_min": GLAT_min,
+                "GLAT_max": GLAT_max,
+                "Ring_catalogue": Ring_catalogue,
+                "data": data,
+                "label_cal": label_cal,
+                "trans_rg": trans_rng,
+            }
+            fl, trans_data, trans_info = ring_augmentation.translation(**trans_params)
+            ## データやlabelの作成に不備があれば、fl=False(例えば、xmin<0や、xmin=xmaxなど)
+            ## 問題がなければ、fl=True
+            if fl:
+                trans_data_ = trans_data.copy()
+                append_data(
+                    processing.norm_res(trans_data_),
+                    trans_info,
+                    mwp_ring_list_val,
+                    frame_mwp_val,
+                )
 
         ####################################
         ## 領域ごとにRingデータを保存していく ###
         ####################################
         if args.each_region:
-            region_frame_mwp_val = pd.DataFrame(region_frame_mwp_val)
-            region_frame_mwp_val["id"] = [_ for _ in range(len(region_frame_mwp_val))]
             savedir_name = "/workspace/val_png/region_val_png/%s" % fits_path
-            if os.path.exists(savedir_name):
-                pass
-            else:
-                os.mkdir(savedir_name)
-
-            ## dataの作成
-            region_mwp_ring_list_val = np.array(region_mwp_ring_list_val).astype(np.float32)
-            for ind in range(region_mwp_ring_list_val.shape[0]):
-                pil_image = Image.fromarray(np.uint8(region_mwp_ring_list_val[ind] * 255))
-                pil_image.save("%s/Ring_%s.png" % (savedir_name, ind))
-
-            ## labelの作成
-            for ind, row in region_frame_mwp_val.iterrows():
-                ll = []
-                if len(row["xmin"]) >= 1:
-                    for la in range(len(row["xmin"])):
-                        ll.append(
-                            {
-                                "Confidence": str(0),
-                                "XMin": str(row["xmin"][la]),
-                                "XMax": str(row["xmax"][la]),
-                                "YMin": str(row["ymin"][la]),
-                                "YMax": str(row["ymax"][la]),
-                            }
-                        )
-                else:
-                    ll.append({"Confidence": str(0)})
-
-                with open("%s/Ring_%s.json" % (savedir_name, ind), "w") as f:
-                    json.dump(ll, f, indent=4)
-
-    #########################################
-    ## デフォルト領域のRingデータを保存していく ###
-    #########################################
-    if not args.each_region:
-        frame_mwp_val = pd.DataFrame(frame_mwp_val)
-        frame_mwp_val["id"] = [i for i in range(len(frame_mwp_val))]
-        savedir_name = "/workspace/val_png/default_val/"
-        if os.path.exists(savedir_name):
-            pass
         else:
-            os.mkdir(savedir_name)
+            savedir_name = "/workspace/val_png/default_val/"
+        os.makedirs(savedir_name, exist_ok=True)
 
         mwp_ring_list_val = np.array(mwp_ring_list_val).astype(np.float32)
         for i in range(mwp_ring_list_val.shape[0]):
             pil_image = Image.fromarray(np.uint8(mwp_ring_list_val[i] * 255))
             pil_image.save("%s/Ring_%s.png" % (savedir_name, i))
 
+        frame_mwp_val = pd.DataFrame(frame_mwp_val)
+        frame_mwp_val["id"] = [i for i in range(len(frame_mwp_val))]
         for i, row in frame_mwp_val.iterrows():
             ll = []
             if len(row["xmin"]) >= 1:
--- scripts/毎epochごとに学習データを作成する/Val_data/processing.py
+++ scripts/毎epochごとに学習データを作成する/Val_data/processing.py
@@ -1,5 +1,6 @@
+import copy
+
 import numpy as np
-import pandas as pd
 import scipy.ndimage
 import torch
 from scipy import signal
@@ -51,6 +52,21 @@ def resize(data, size):
     resize_data_ = np.swapaxes(resize_data, 0, 1)
     resize_data_ = np.swapaxes(resize_data_, 1, 2)
     return resize_data_
+
+
+def norm_res(data):
+    """
+    データを切り取り、
+    normalizeとresizeをする。
+    """
+    # shape_y = data.shape[0]
+    # shape_x = data.shape[1]
+    # data = data[int(shape_y / 4) : int(shape_y * 3 / 4), int(shape_x / 4) : int(shape_x * 3 / 4)]
+    data_ = copy.deepcopy(data)
+    data_ = normalize(data_)
+    data_ = resize(data_, 300)
+
+    return data_
 
 
 def conv(obj_size, obj_sig, data):
