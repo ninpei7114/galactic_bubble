74be2dac1d5282df74566e09c55cbc905023930d
shimpei <ninpei7114@gmail.com>
2023-08-21 14:00:46 +0900

    formatterでインデント等の修正

--- scripts/training_based_on_translation_NoRing_cluster/Non_Ring/clustering.py
+++ scripts/training_based_on_translation_NoRing_cluster/Non_Ring/clustering.py
@@ -6,7 +6,8 @@ from PIL import Image
 from sklearn.cluster import KMeans
 
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 from utils.ssd_model import SSD
 
 import argparse
@@ -23,45 +24,39 @@ example command:
 python clustering.py 7 /workspace/earlystopping.pth /workspace/NonRing_png/region_NonRing_png
 """
 
+
 def parse_args():
-
     ## クラス数、モデルのcheckpoint、Non-Ring画像の場所
-    parser = argparse.ArgumentParser(description='make data for SSD')
-    parser.add_argument('class_num',  help='clusteringの個数 (default: 9)', 
-                        default=9)
-    parser.add_argument('model_checkpoint',  help='特徴量を生成するためのモデルのcheckpointの場所')
-    parser.add_argument('NonRing_dir',  help='NonRing画像の場所')
-
+    parser = argparse.ArgumentParser(description="make data for SSD")
+    parser.add_argument("class_num", help="clusteringの個数 (default: 9)", default=9)
+    parser.add_argument("model_checkpoint", help="特徴量を生成するためのモデルのcheckpointの場所")
+    parser.add_argument("NonRing_dir", help="NonRing画像の場所")
 
     return parser.parse_args()
 
 
-
 def main(args):
-    
     #################################
     ## クラスタリングのモデルを読み込む ##
     #################################
 
     torch.backends.cudnn.benchmark = True
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    print('Loading Model....')
-    net_weights = torch.load(
-        args.model_checkpoint)
-
+    print("Loading Model....")
+    net_weights = torch.load(args.model_checkpoint)
 
     input_size = 300
 
     ssd_cfg = {
-        'num_classes': 2,  # 背景クラスを含めた合計クラス数
-        'input_size': input_size,  # 画像の入力サイズ
-        'bbox_aspect_num': [4, 6, 6, 6, 4, 4],  # 出力するDBoxのアスペクト比の種類
-        'feature_maps': [38, 19, 10, 5, 3, 1],  # 各sourceの画像サイズ   
-        'steps': [8, 16, 32, 64, 100, 300],  # DBOXの大きさを決める
-        'min_sizes': [30, 60, 111, 162, 213, 264],  # DBOXの大きさを決める
-        'max_sizes': [60, 111, 162, 213, 264, 315],  # DBOXの大きさを決める
-        'aspect_ratios': [[2], [2,3], [2,3], [2,3], [2], [2]],
+        "num_classes": 2,  # 背景クラスを含めた合計クラス数
+        "input_size": input_size,  # 画像の入力サイズ
+        "bbox_aspect_num": [4, 6, 6, 6, 4, 4],  # 出力するDBoxのアスペクト比の種類
+        "feature_maps": [38, 19, 10, 5, 3, 1],  # 各sourceの画像サイズ
+        "steps": [8, 16, 32, 64, 100, 300],  # DBOXの大きさを決める
+        "min_sizes": [30, 60, 111, 162, 213, 264],  # DBOXの大きさを決める
+        "max_sizes": [60, 111, 162, 213, 264, 315],  # DBOXの大きさを決める
+        "aspect_ratios": [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
     }
 
     net_w = SSD(cfg=ssd_cfg)
@@ -76,63 +71,62 @@ def main(args):
     net_w = net_w.vgg
     net_w.to(device)
 
-    print('Loading data....')
+    print("Loading data....")
     ## データの取得と形成
-    path_list = sorted(glob.glob('%s/*/*.png'%args.NonRing_dir))
+    path_list = sorted(glob.glob("%s/*/*.png" % args.NonRing_dir))
     data = []
     for i in path_list:
         data.append(np.array(Image.open(i)))
 
-    data = np.array(data)[:,:,:,:2]
+    data = np.array(data)[:, :, :, :2]
     features_list = []
 
     ####################################################
     ## 画像をモデルに入力し、得られた特徴量をクラスタリングする ##
     ####################################################
 
-    print('Clustering')
-    batch = np.linspace(0, data.shape[0], int(data.shape[0]/32))
+    print("Clustering")
+    batch = np.linspace(0, data.shape[0], int(data.shape[0] / 32))
 
     ## データをモデルに入力する
-    for i in tqdm.tqdm(range(len(batch)-1)):
-        x = data[int(batch[i]):int(batch[i+1])]
+    for i in tqdm.tqdm(range(len(batch) - 1)):
+        x = data[int(batch[i]) : int(batch[i + 1])]
         p_data = torch.from_numpy(x)
-        p_data = p_data.permute(0 ,3, 1, 2)
+        p_data = p_data.permute(0, 3, 1, 2)
 
         with torch.no_grad():
             net_w.eval()
             p_data = p_data.to(device, dtype=torch.float)
             for k in range(51):
                 p_data = net_w[k](p_data)
-            features_list.append(p_data.to('cpu').detach().numpy().copy())
-
+            features_list.append(p_data.to("cpu").detach().numpy().copy())
 
     ## 特徴量をクラスタリング
     features_list = np.concatenate(features_list)
-    prediction = KMeans(n_clusters=int(args.class_num), random_state=123).fit_predict(features_list.reshape(features_list.shape[0], -1))
-
+    prediction = KMeans(n_clusters=int(args.class_num), random_state=123).fit_predict(
+        features_list.reshape(features_list.shape[0], -1)
+    )
 
     ####################################
     ## Non-Ringデータをクラスごとに分ける ##
     ####################################
 
-    print('Move photo')
-    for i in glob.glob('%s/*'%args.NonRing_dir):
+    print("Move photo")
+    for i in glob.glob("%s/*" % args.NonRing_dir):
         for k in range(int(args.class_num)):
-            if os.path.exists('%s/class%s'%(i, k)):
+            if os.path.exists("%s/class%s" % (i, k)):
                 pass
             else:
-                os.mkdir('%s/class%s'%(i, k))
+                os.mkdir("%s/class%s" % (i, k))
 
-    
     for path, pred in zip(path_list, prediction):
         photo = path
-        json = path[:-4]+'.json'
-        
-        shutil.move(photo, '/'.join(photo.split('/')[:-1])+'/class%s'%pred)
-        shutil.move(json, '/'.join(photo.split('/')[:-1])+'/class%s'%pred)
+        json = path[:-4] + ".json"
+
+        shutil.move(photo, "/".join(photo.split("/")[:-1]) + "/class%s" % pred)
+        shutil.move(json, "/".join(photo.split("/")[:-1]) + "/class%s" % pred)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     main(args)
--- scripts/training_based_on_translation_NoRing_cluster/Non_Ring/proceesing.py
+++ scripts/training_based_on_translation_NoRing_cluster/Non_Ring/proceesing.py
@@ -7,18 +7,16 @@ import torch
 from torch.nn import functional as F
 
 
-
 def norm(data):
     min_ = np.min(data)
     b = np.std(data)
     mean = np.mean(data)
     data -= min_
-    max_ = b*3 + mean
-    data[data>max_] = max_
+    max_ = b * 3 + mean
+    data[data > max_] = max_
 
     data /= np.max(data)
     return data
-
 
 
 def normalize(array):
@@ -29,13 +27,12 @@ def normalize(array):
     gauss_list = []
     s = array.shape[2]
     for k in range(s):
-        cut_data_k = array[:,:,k]
+        cut_data_k = array[:, :, k]
         cut_data_k = norm(cut_data_k)
-        gauss_list.append(cut_data_k[:,:,None])                           
+        gauss_list.append(cut_data_k[:, :, None])
 
     cut_data = np.concatenate(gauss_list, axis=2)
     return cut_data
-
 
 
 def resize(data, size):
@@ -49,13 +46,12 @@ def resize(data, size):
     cut_data = np.swapaxes(cut_data, 0, 1)
     cut_data = torch.from_numpy(cut_data)
     cut_data = cut_data.unsqueeze(0)
-    resize_data = F.interpolate(cut_data, (size, size), mode='bilinear', align_corners=False)
+    resize_data = F.interpolate(cut_data, (size, size), mode="bilinear", align_corners=False)
     resize_data = np.squeeze(resize_data.detach().numpy())
-    
+
     resize_data_ = np.swapaxes(resize_data, 0, 1)
     resize_data_ = np.swapaxes(resize_data_, 1, 2)
     return resize_data_
-
 
 
 def conv(obj_size, obj_sig, data):
@@ -68,19 +64,19 @@ def conv(obj_size, obj_sig, data):
     小さければ、そのまま返す。
     """
 
-    if data.shape[0]>obj_size:
-        fwhm = (data.shape[0]/obj_size)*2
-        sig3 = fwhm/(2*(2*np.log(2))**(1/2))
-        sig2 = (sig3**2-obj_sig**2)**(1/2)
+    if data.shape[0] > obj_size:
+        fwhm = (data.shape[0] / obj_size) * 2
+        sig3 = fwhm / (2 * (2 * np.log(2)) ** (1 / 2))
+        sig2 = (sig3**2 - obj_sig**2) ** (1 / 2)
 
-        kernel = np.outer(signal.gaussian(8*round(sig2)+1, sig2), signal.gaussian(8*round(sig2)+1, sig2))
-        kernel1= kernel/np.sum(kernel)
+        kernel = np.outer(signal.gaussian(8 * round(sig2) + 1, sig2), signal.gaussian(8 * round(sig2) + 1, sig2))
+        kernel1 = kernel / np.sum(kernel)
 
         conv_list = []
         for k in range(data.shape[2]):
-            cut_data_k = data[:,:,k]
-            lurred_k = signal.fftconvolve(cut_data_k, kernel1, mode='same')
-            conv_list.append(lurred_k[:,:,None])
+            cut_data_k = data[:, :, k]
+            lurred_k = signal.fftconvolve(cut_data_k, kernel1, mode="same")
+            conv_list.append(lurred_k[:, :, None])
 
         pi = np.concatenate(conv_list, axis=2)
     else:
@@ -88,32 +84,32 @@ def conv(obj_size, obj_sig, data):
     return pi
 
 
-
 def remove_nan(data1):
     # fits???????????????max?????s
-    mask1_10 = (data1==data1)
+    mask1_10 = data1 == data1
     mask1_1010 = np.where(mask1_10, 0, 1)
     label1, name1 = scipy.ndimage.label(mask1_1010)
-    data_areas1 = scipy.ndimage.sum(mask1_1010, label1, np.arange(name1+1))
+    data_areas1 = scipy.ndimage.sum(mask1_1010, label1, np.arange(name1 + 1))
     minsize1 = 70000
-    data_mask1_10 =  (data_areas1 < minsize1)&(0 < data_areas1)
+    data_mask1_10 = (data_areas1 < minsize1) & (0 < data_areas1)
     small_mask1_10 = data_mask1_10[label1.ravel()].reshape(label1.shape)
     data1[small_mask1_10] = np.nanmax(data1)
 
     return data1
 
 
-
 from PIL import Image, ImageDraw, ImageFont
+
+
 def data_view_rectangl(col, imgs, infos=None, moji_size=100):
-    '''
+    """
     col: number of columns
     imgs: tensor or nparray with a shape of (?, y, x, 1) or (?, y, x, 3)
     infos: dictonary from CutTable
-    '''
-    imgs = np.uint8(imgs[:, ::-1, :, 0]) if imgs.shape[3] == 1 else np.uint8(imgs[:,::-1])
-    row = (lambda x, y: x//y if x/y-x//y==0.0 else x//y+1)(imgs.shape[0], col)
-    dst = Image.new('RGB', (imgs.shape[1]*col, imgs.shape[2]*row))
+    """
+    imgs = np.uint8(imgs[:, ::-1, :, 0]) if imgs.shape[3] == 1 else np.uint8(imgs[:, ::-1])
+    row = (lambda x, y: x // y if x / y - x // y == 0.0 else x // y + 1)(imgs.shape[0], col)
+    dst = Image.new("RGB", (imgs.shape[1] * col, imgs.shape[2] * row))
 
     # font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', moji_size)
     for i, arr in enumerate(imgs):
@@ -122,11 +118,18 @@ def data_view_rectangl(col, imgs, infos=None, moji_size=100):
         if infos is not None:
             draw = ImageDraw.Draw(img)
             # draw.text((10, 10), '%s'%infos['id'].tolist()[i], font=font)
-            for j in range(len(infos['xmin'].tolist()[i])):
-                draw.rectangle((infos['xmin'].tolist()[i][j]*300, (1 - infos['ymax'].tolist()[i][j])*300,
-                            infos['xmax'].tolist()[i][j]*300, (1 - infos['ymin'].tolist()[i][j])*300), width=2)
+            for j in range(len(infos["xmin"].tolist()[i])):
+                draw.rectangle(
+                    (
+                        infos["xmin"].tolist()[i][j] * 300,
+                        (1 - infos["ymax"].tolist()[i][j]) * 300,
+                        infos["xmax"].tolist()[i][j] * 300,
+                        (1 - infos["ymin"].tolist()[i][j]) * 300,
+                    ),
+                    width=2,
+                )
 
-        quo, rem = i//col, i%col
-        dst.paste(img, (arr.shape[0]*rem, arr.shape[1]*quo))
+        quo, rem = i // col, i % col
+        dst.paste(img, (arr.shape[0] * rem, arr.shape[1] * quo))
 
     return dst
