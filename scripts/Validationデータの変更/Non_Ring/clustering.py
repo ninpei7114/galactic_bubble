851acf5c9ace1bf047e4c25530700348f233752d
shimpei <ninpei7114@gmail.com>
2023-08-25 16:08:48 +0900

    フォーマットの整形と不要箇所の削除

--- scripts/Validationデータの変更/Non_Ring/clustering.py
+++ scripts/Validationデータの変更/Non_Ring/clustering.py
@@ -1,23 +1,18 @@
-import torch
-import torch.nn as nn
+import argparse
+import glob
+import os
+import shutil
+import sys
 
 import numpy as np
-from numpy.random import default_rng
+import torch
+import torch.nn as nn
+import tqdm
 from PIL import Image
 from sklearn.cluster import KMeans
-from skimage import transform
-
-import sys
 
 sys.path.append("../")
 from utils.ssd_model import SSD
-
-import argparse
-import glob
-import tqdm
-import copy
-import shutil
-
 
 """
 Non-Ringをクラスタリングするためのスクリプト
@@ -47,12 +42,9 @@ def main(args):
 
     print("Loading Model....")
     net_weights = torch.load(args.model_checkpoint)
-
-    input_size = 300
-
     ssd_cfg = {
         "num_classes": 2,  # 背景クラスを含めた合計クラス数
-        "input_size": input_size,  # 画像の入力サイズ
+        "input_size": 300,  # 画像の入力サイズ
         "bbox_aspect_num": [4, 6, 6, 6, 4, 4],  # 出力するDBoxのアスペクト比の種類
         "feature_maps": [38, 19, 10, 5, 3, 1],  # 各sourceの画像サイズ
         "steps": [8, 16, 32, 64, 100, 300],  # DBOXの大きさを決める
@@ -60,16 +52,13 @@ def main(args):
         "max_sizes": [60, 111, 162, 213, 264, 315],  # DBOXの大きさを決める
         "aspect_ratios": [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
     }
-
     net_w = SSD(cfg=ssd_cfg)
     net_w.load_state_dict(net_weights)
-
     ## vggとextraで構成されるモデルを構築
     ## 特徴量サイズ、1x1x256
     for i in net_w.extras:
         net_w.vgg.append(i)
         net_w.vgg.append(nn.ReLU())
-
     net_w = net_w.vgg
     net_w.to(device)
 
@@ -79,17 +68,15 @@ def main(args):
     data = []
     for i in path_list:
         data.append(np.array(Image.open(i)))
-
     data = np.array(data)[:, :, :, :2]
-    features_list = []
 
     ####################################################
     ## 画像をモデルに入力し、得られた特徴量をクラスタリングする ##
     ####################################################
-
     print("Clustering")
-    batch = np.linspace(0, data.shape[0], int(data.shape[0] / 32))
 
+    features_list = []
+    batch = np.linspace(0, data.shape[0], int(data.shape[0] / 32))
     ## データをモデルに入力する
     for i in tqdm.tqdm(range(len(batch) - 1)):
         x = data[int(batch[i]) : int(batch[i + 1])]
@@ -105,34 +92,23 @@ def main(args):
 
     ## 特徴量をクラスタリング
     features_list = np.concatenate(features_list)
-    prediction = KMeans(n_clusters=int(args.class_num), random_state=123).fit_predict(
+    prediction = KMeans(n_clusters=int(args.class_num), random_state=123, n_init="auto").fit_predict(
         features_list.reshape(features_list.shape[0], -1)
     )
+    #################################
+    ## Non-Ringデータをクラスごとに移動##
+    #################################
+    for i in glob.glob("%s/*" % args.NonRing_dir):
+        for k in range(int(args.class_num)):
+            os.makedirs("%s/class%s" % (i, k), exist_ok=True)
 
-    ####################################
-    ## Non-Ringデータをクラスごとに分ける ##
-    ####################################
-    NonRing_random_uni = default_rng(123)
-    count = 0
+    print("Move photo")
     for path, pred in zip(path_list, prediction):
         photo = path
         json = path[:-4] + ".json"
 
-        if pred == 3:
-            photo_data = np.array(Image.open(photo))
-
-            for aug in NonRing_random_uni.integers(0, 2, 2):
-                if aug == 0:
-                    tempo = copy.deepcopy(photo_data)
-                    ud = np.flipud(tempo)
-                    count += 1
-                elif aug == 1:
-                    lr = np.fliplr(tempo)
-                else:
-                    rotate_cut_data = transform.rotate(tempo, NonRing_random_uni.integers(0, [90, 180, 270], 1))
-
-        shutil.move(photo, "/".join(photo.split("/")[:-1]) + "/class%s" % pred)
-        shutil.move(json, "/".join(photo.split("/")[:-1]) + "/class%s" % pred)
+        shutil.move(photo, "/".join(photo.split("/")[:-1]) + f"/class{pred}/" + photo.split("/")[-1])
+        shutil.move(json, "/".join(json.split("/")[:-1]) + f"/class{pred}/" + json.split("/")[-1])
 
 
 if __name__ == "__main__":
