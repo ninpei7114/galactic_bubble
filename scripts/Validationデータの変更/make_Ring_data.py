d0f4b49bf46f3f0c013dfc14492a23e193d91f46
shimpei <ninpei7114@gmail.com>
2023-08-29 22:09:24 +0900

    png, jsonファイルをデータ生成ごとに作成するように変更

--- scripts/Validationデータの変更/make_Ring_data.py
+++ scripts/Validationデータの変更/make_Ring_data.py
@@ -1,4 +1,5 @@
 import copy
+import glob
 import json
 import os
 
@@ -15,18 +16,28 @@ import ring_augmentation
 
 
 def make_ring(savedir_name, train_cfg, args, train_l, trans_rng, epoch, save_data_path):
-    sig1 = 1 / (2 * (np.log(2)) ** (1 / 2))
+    """trainingに使用するRingを作成する関数
 
-    ## choice catalogue from 'CH' or 'MWP'
-    choice_catalogue = "CH"
+    Args:
+        savedir_name (str): labelとring_pdfを保存するpath
+        train_cfg (dictionary): augmentationの種類
+        args (args): args
+        train_l (list): trainingに使用するfitsのリスト
+        trans_rng (numpy default_rng): numpy default_rng
+        epoch (int): epochの上限
+        save_data_path (str): 学習データを保存するpath
+
+    Returns:
+        None
+    """
+    sig1 = 1 / (2 * (np.log(2)) ** (1 / 2))
+    choice_catalogue = "CH"  # choice catalogue from 'CH' or 'MWP'
     Ring_CATALOGUE = ring_augmentation.catalogue(choice_catalogue)
     pbar = tqdm.tqdm(range(len(train_l)))
     flip = train_cfg["flip"]
     rot = train_cfg["rotate"]
-    scale = train_cfg["scale"]
     translation = train_cfg["translation"]
-
-    frame_mwp_train, mwp_ring_list_train = [], []
+    frame_mwp_train, count = [], 0
 
     for i in pbar:
         pbar.set_description(train_l[i])
@@ -35,7 +46,6 @@ def make_ring(savedir_name, train_cfg, args, train_l, trans_rng, epoch, save_dat
         spitzer_gfits = astropy.io.fits.open(args.spitzer_path + "/" + fits_path + "/" + "g.fits")[0]
         spitzer_bfits = astropy.io.fits.open(args.spitzer_path + "/" + fits_path + "/" + "b.fits")[0]
 
-        ## RGBにしたいため、fitsのdataを重ねる
         data = np.concatenate(
             [
                 processing.remove_nan(spitzer_rfits.data[:, :, None]),
@@ -45,206 +55,164 @@ def make_ring(savedir_name, train_cfg, args, train_l, trans_rng, epoch, save_dat
             axis=2,
         )
 
+        #####################################
+        ## fits範囲のChurchwellカタログを取得 ##
+        #####################################
         a = data.shape[0]
         b = data.shape[1]
         w = astropy.wcs.WCS(spitzer_rfits.header)
-
         GLON_min, GLAT_min = w.all_pix2world(b, 0, 0)
         GLON_max, GLAT_max = w.all_pix2world(0, a, 0)
-        GLON_center = (GLON_min + GLON_max) / 2
-        GLON_new_min = GLON_center - 1.5
-        GLON_new_max = GLON_center + 1.5
+        Ring_catalogue = Ring_CATALOGUE.query("@GLON_min < GLON <= @GLON_max")
 
-        Ring_catalogue = Ring_CATALOGUE.query("@GLON_new_min < GLON <= @GLON_new_max")
-
-        ## star_listは辞書
         label_cal = label_caliculator.label_caliculator(choice_catalogue, w)
         label_cal.all_star(Ring_catalogue)
 
         for _, row in Ring_catalogue.iterrows():
             x_pix_min, y_pix_min, x_pix_max, y_pix_max, flag = label_cal.calc_pix(
-                row, GLON_new_min, GLON_new_max, GLAT_min, GLAT_max, 1
+                row, GLON_min, GLON_max, GLAT_min, GLAT_max, 1
             )
-            if flag:  # calc_pix時に100回試行してもできなかった場合の場合分け
+            if flag or x_pix_min < 0 or y_pix_min < 0:  # calc_pix時に100回試行してもできなかった場合の場合分け
                 label_cal.find_cover()
-                if x_pix_min < 0 or y_pix_min < 0:
+
+                c_data = data[int(y_pix_min) : int(y_pix_max), int(x_pix_min) : int(x_pix_max)].view()
+                cut_data = copy.deepcopy(c_data)
+                pi = processing.conv(300, sig1, cut_data)
+                label_cal.make_label(Ring_catalogue)
+                r_shape_y = pi.shape[0]
+                r_shape_x = pi.shape[1]
+                res_data = pi[
+                    int(r_shape_y / 52) : int(r_shape_y * 51 / 52), int(r_shape_x / 52) : int(r_shape_x * 51 / 52)
+                ]
+                if np.isnan(res_data.sum()) or np.std(res_data[:, :, 0]) < 1e-9:
                     pass
-
                 else:
-                    c_data = data[int(y_pix_min) : int(y_pix_max), int(x_pix_min) : int(x_pix_max)].view()
-                    cut_data = copy.deepcopy(c_data)
-                    pi = processing.conv(300, sig1, cut_data)
-                    label_cal.make_label(Ring_catalogue)
-                    r_shape_y = pi.shape[0]
-                    r_shape_x = pi.shape[1]
-                    res_data = pi[
-                        int(r_shape_y / 52) : int(r_shape_y * 51 / 52), int(r_shape_x / 52) : int(r_shape_x * 51 / 52)
-                    ]
-                    if np.isnan(res_data.sum()):
-                        pass
-                    else:
-                        ########################
-                        ## 普通に切り出したリング ##
-                        ########################
-                        xmin_list, ymin_list, xmax_list, ymax_list, name_list = label_cal.check_list()
-                        info = {
-                            "fits": fits_path,
-                            "name": name_list,
-                            "xmin": xmin_list,
-                            "xmax": xmax_list,
-                            "ymin": ymin_list,
-                            "ymax": ymax_list,
+                    ########################
+                    ## 普通に切り出したリング ##
+                    ########################
+                    info = label_cal.check_list()
+                    info["fits"] = fits_path
+                    count = make_png_and_json(save_data_path, count, processing.norm_res(res_data), info)
+                    frame_mwp_train.append(info)
+                    #######################
+                    ## Ring augmentation ##
+                    #######################
+                    for _ in range(args.augmentation_ratio):
+                        label_cal_for_trans = label_caliculator.label_caliculator(choice_catalogue, w)
+                        label_cal_for_trans.all_star(Ring_catalogue)
+                        trans_params = {
+                            "row": row,
+                            "fits_path": fits_path,
+                            "GLON_min": GLON_min,
+                            "GLON_max": GLON_max,
+                            "GLAT_min": GLAT_min,
+                            "GLAT_max": GLAT_max,
+                            "Ring_catalogue": Ring_catalogue,
+                            "data": data,
+                            "label_cal": label_cal_for_trans,
+                            "trans_rg": trans_rng,
                         }
-                        append_data(
-                            processing.norm_res(res_data),
-                            info,
-                            mwp_ring_list_train,
-                            frame_mwp_train,
-                        )
 
-                        #######################
-                        ## Ring augmentation ##
-                        #######################
-                        for _ in range(args.augmentation_ratio):
-                            label_cal_for_trans = label_caliculator.label_caliculator(choice_catalogue, w)
-                            label_cal_for_trans.all_star(Ring_catalogue)
-                            trans_params = {
-                                "row": row,
-                                "fits_path": fits_path,
-                                "GLON_new_min": GLON_new_min,
-                                "GLON_new_max": GLON_new_max,
-                                "GLAT_min": GLAT_min,
-                                "GLAT_max": GLAT_max,
-                                "Ring_catalogue": Ring_catalogue,
-                                "data": data,
-                                "label_cal": label_cal_for_trans,
-                                "trans_rg": trans_rng,
-                            }
-
-                            ###### 並行移動 ######
+                        ###### 並行移動 ######
+                        if translation:
+                            fl, trans_data, trans_info = ring_augmentation.translation(**trans_params)
+                            ## データやlabelの作成に不備があれば、fl=False(例えば、xmin<0や、xmin=xmaxなど)
+                            ## 問題がなければ、fl=True
+                            if fl:
+                                trans_data_ = trans_data.copy()
+                                count = make_png_and_json(
+                                    save_data_path, count, processing.norm_res(trans_data_), trans_info
+                                )
+                                frame_mwp_train.append(trans_info)
+                        ###### 回転 ######
+                        if rot:
                             if translation:
-                                fl, trans_data, trans_info = ring_augmentation.translation(**trans_params)
-                                ## データやlabelの作成に不備があれば、fl=False(例えば、xmin<0や、xmin=xmaxなど)
-                                ## 問題がなければ、fl=True
                                 if fl:
-                                    trans_data_ = trans_data.copy()
-                                    append_data(
-                                        processing.norm_res(trans_data_),
-                                        trans_info,
-                                        mwp_ring_list_train,
-                                        frame_mwp_train,
-                                    )
-
-                            ###### 回転 ######
-                            if rot:
-                                if translation:
-                                    if fl:
-                                        for deg in [90, 180, 270]:
-                                            rot_data, rotate_info = ring_augmentation.rotate_data(
-                                                deg, trans_data, trans_info
-                                            )
-                                            append_data(
-                                                processing.norm_res(rot_data),
-                                                rotate_info,
-                                                mwp_ring_list_train,
-                                                frame_mwp_train,
-                                            )
-                                    else:
-                                        pass
-                                else:
                                     for deg in [90, 180, 270]:
-                                        rot_data, rotate_info = ring_augmentation.rotate_data(deg, res_data, info)
-                                        append_data(
-                                            processing.norm_res(rot_data),
-                                            rotate_info,
-                                            mwp_ring_list_train,
-                                            frame_mwp_train,
+                                        rot_data, rotate_info = ring_augmentation.rotate_data(
+                                            deg, trans_data, trans_info
                                         )
-
-                            ###### 上下反転 ######
-                            if flip:
-                                if translation:
-                                    if fl:
-                                        ud_res_data, lr_res_data, ud_info, lr_info = ring_augmentation.flip_data(
-                                            trans_data, trans_info
+                                        count = make_png_and_json(
+                                            save_data_path, count, processing.norm_res(rot_data), rotate_info
                                         )
-                                        append_data(
-                                            processing.norm_res(ud_res_data),
-                                            ud_info,
-                                            mwp_ring_list_train,
-                                            frame_mwp_train,
-                                        )
-                                        append_data(
-                                            processing.norm_res(lr_res_data),
-                                            lr_info,
-                                            mwp_ring_list_train,
-                                            frame_mwp_train,
-                                        )
-                                    else:
-                                        pass
+                                        frame_mwp_train.append(rotate_info)
                                 else:
+                                    pass
+                            else:
+                                for deg in [90, 180, 270]:
+                                    rot_data, rotate_info = ring_augmentation.rotate_data(deg, res_data, info)
+                                    count = make_png_and_json(
+                                        save_data_path, count, processing.norm_res(rot_data), rotate_info
+                                    )
+                                    frame_mwp_train.append(rotate_info)
+                        ###### 上下反転 ######
+                        if flip:
+                            if translation:
+                                if fl:
                                     ud_res_data, lr_res_data, ud_info, lr_info = ring_augmentation.flip_data(
-                                        res_data, info
+                                        trans_data, trans_info
                                     )
-                                    append_data(
-                                        processing.norm_res(ud_res_data),
-                                        ud_info,
-                                        mwp_ring_list_train,
-                                        frame_mwp_train,
+                                    count = make_png_and_json(
+                                        save_data_path, count, processing.norm_res(ud_res_data), ud_info
                                     )
-                                    append_data(
-                                        processing.norm_res(lr_res_data),
-                                        lr_info,
-                                        mwp_ring_list_train,
-                                        frame_mwp_train,
+                                    count = make_png_and_json(
+                                        save_data_path, count, processing.norm_res(lr_res_data), lr_info
                                     )
+                                    frame_mwp_train.append(lr_info)
+                                    frame_mwp_train.append(ud_info)
+                                else:
+                                    pass
+                            else:
+                                ud_res_data, lr_res_data, ud_info, lr_info = ring_augmentation.flip_data(
+                                    res_data, info
+                                )
+                                count = make_png_and_json(
+                                    save_data_path, count, processing.norm_res(ud_res_data), ud_info
+                                )
+                                count = make_png_and_json(
+                                    save_data_path, count, processing.norm_res(lr_res_data), lr_info
+                                )
+                                frame_mwp_train.append(lr_info)
+                                frame_mwp_train.append(ud_info)
 
+    ##################################
+    ## 学習に用いるリングのpdf画像を作成 ##
+    ##################################
     frame_mwp_train = pd.DataFrame(frame_mwp_train)
     frame_mwp_train["id"] = [i for i in range(len(frame_mwp_train))]
-    mwp_ring_list_train = np.array(mwp_ring_list_train).astype(np.float32)
-    os.makedirs(savedir_name, exist_ok=True)
+    choice_ind = np.sort(trans_rng.choice(count, size=90, replace=False))
+    mwp_ring_list_train = []
+    for i in choice_ind:
+        mwp_ring_list_train.append(np.array(Image.open(save_data_path + f"/train/ring/Ring_{i}.png")))
 
-    mwp_ring_list_train_ = np.uint8(mwp_ring_list_train * 255)
-    if mwp_ring_list_train_.shape[0] > 3000:
-        slice = 10
-    else:
-        slice = 1
     os.makedirs(savedir_name + "/train_ring_pdf", exist_ok=True)
-    processing.data_view_rectangl(25, mwp_ring_list_train_[::slice], frame_mwp_train[::slice]).save(
+    processing.data_view_rectangl(25, np.array(mwp_ring_list_train), frame_mwp_train.iloc[choice_ind]).save(
         savedir_name + "/train_ring_pdf" + f"/train_ring_{epoch}.pdf"
     )
     frame_mwp_train.to_csv(savedir_name + "/train_label.csv")
 
-    print("convert photo....")
-    ## Trainingデータをpngファイルに変換＋保存
-    for i, ring_data in enumerate(mwp_ring_list_train):
-        pil_image = Image.fromarray(np.uint8(ring_data * 255))
-        pil_image.save(f"{save_data_path}/train/ring/Ring_{i}.png")
 
-    ## Training labelをjsonに変換＋保存
-    for i, row in frame_mwp_train.iterrows():
-        ll = []
-        if len(row["xmin"]) >= 1:
-            for la in range(len(row["xmin"])):
-                ll.append(
-                    {
-                        "Confidence": str(0),
-                        "XMin": str(row["xmin"][la]),
-                        "XMax": str(row["xmax"][la]),
-                        "YMin": str(row["ymin"][la]),
-                        "YMax": str(row["ymax"][la]),
-                    }
-                )
-        else:
-            pass
+def make_png_and_json(save_data_path, count, ring_data, info):
+    pil_image = Image.fromarray(np.uint8(ring_data * 255))
+    pil_image.save(f"{save_data_path}/train/ring/Ring_{count}.png")
 
-        with open(f"{save_data_path}/train/ring/Ring_{i}.json", "w") as f:
-            json.dump(ll, f, indent=4)
+    ll = []
+    if len(info["xmin"]) >= 1:
+        for la in range(len(info["xmin"])):
+            ll.append(
+                {
+                    "Confidence": str(0),
+                    "XMin": str(info["xmin"][la]),
+                    "XMax": str(info["xmax"][la]),
+                    "YMin": str(info["ymin"][la]),
+                    "YMax": str(info["ymax"][la]),
+                }
+            )
+    else:
+        pass
+    with open(f"{save_data_path}/train/ring/Ring_{count}.json", "w") as f:
+        json.dump(ll, f, indent=4)
 
-    return mwp_ring_list_train, frame_mwp_train
+    count += 1
 
-
-def append_data(data, info, data_list, frame):
-    if not np.isnan(data.sum()):
-        data_list.append(data)
-        frame.append(info)
+    return count
