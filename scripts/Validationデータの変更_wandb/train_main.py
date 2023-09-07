5ead9f3436a17ba7620ceb0a00c7fa58c3807999
shimpei <ninpei7114@gmail.com>
2023-09-07 17:32:37 +0900

    wandbの位置をtrain_modelから移行

--- scripts/Validationデータの変更_wandb/train_main.py
+++ scripts/Validationデータの変更_wandb/train_main.py
@@ -4,13 +4,16 @@ import os
 from itertools import product as product
 from math import sqrt as sqrt
 
+import numpy as np
 from PIL import ImageFile
 import torch
 import torch.optim as optim
+import wandb
 
 from train_model import train_model
 from training_sub import print_and_log, weights_init
 from utils.ssd_model import SSD, MultiBoxLoss
+import l18_infer
 
 
 def parse_args():
@@ -51,7 +54,8 @@ def parse_args():
     # NonRing
     parser.add_argument("--NonRing_class_num", type=int, default=8)
     parser.add_argument("--NonRing_remove_class_list", nargs="*", type=int, default=[6])
-    parser.add_argument("--NonRing_aug_num", nargs="*", type=int, default=[5, 2, 1, 0, 0, 3, 1])
+    parser.add_argument("--NonRing_aug_num", nargs="*", type=int, default=[5, 2, 1, 0, 0, 0, 3, 1])
+    parser.add_argument("--Val_remove_size_list", nargs="*", type=int, default=[])
 
     return parser.parse_args()
 
@@ -73,6 +77,9 @@ def main(args):
     ImageFile.LOAD_TRUNCATED_IMAGES = True
 
     os.makedirs(args.savedir_path, exist_ok=True)
+    default_val_size = np.array([150, 300, 600, 900, 1200, 1800, 2500, 3000])
+    for remove_size in args.Val_remove_size_list:
+        default_val_size = default_val_size[default_val_size != remove_size]
 
     ## 上下反転、回転、縮小、平行移動の4パターンの組み合わせでaugmentationをする。
     flip_list = [True]  # , False]
@@ -108,10 +115,19 @@ def main(args):
             "====================================",
         ]
         print_and_log(f_log, log_list)
+        run = wandb.init(
+            project=args.wandb_project,
+            name=f"リング選定 : {args.ring_select}",
+            config={
+                "learning_rate": args.lr,
+                "weight_decay": args.weight_decay,
+                "fits_index": args.fits_index,
+                "n_splits": args.n_splits,
+                "fits_random_state": args.fits_random_state,
+            },
+        )
+        wandb.watch(net, log_freq=100)
 
-        ####################
-        ## Training Model ##
-        ####################
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print_and_log(f_log, f"使用デバイス： {device}")
 
@@ -138,11 +154,36 @@ def main(args):
             "args": args,
             "train_cfg": train_cfg,
             "device": device,
+            "run": run,
+            "val_size": default_val_size,
         }
 
+        ####################
+        ## Training Model ##
+        ####################
         train_model(**train_model_params)
 
+        artifact = wandb.Artifact("model", type="model")
+        artifact.add_dir(name)
+        run.log_artifact(artifact, aliases=["latest", "best"])
         f_log.close()
+
+        # l18領域の推論
+        if args.l18_infer:
+            f1_score, pre, re, conf_thre = l18_infer.infer_l18(name, args, default_val_size)
+            print_and_log(
+                f_log,
+                [f"l18 F1 score: {f1_score}", f"precision: {pre}", f"recall: {re}", f"conf_threshold: {conf_thre}"],
+            )
+            wandb.run.summary["l18_f1_score"] = f1_score
+            wandb.run.summary["l18_precision"] = pre
+            wandb.run.summary["l18_recall"] = re
+            wandb.run.summary["l18_conf_threshold"] = conf_thre
+        else:
+            pass
+
+        wandb.alert(title="学習が終了しました", text="学習が終了しました")
+        wandb.finish()
 
 
 if __name__ == "__main__":
