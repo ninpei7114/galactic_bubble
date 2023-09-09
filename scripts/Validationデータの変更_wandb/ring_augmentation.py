8cdcd4e4cb1051daa2cd8735d7436428314cc2d1
shimpei <ninpei7114@gmail.com>
2023-09-10 08:29:01 +0900

    rank_2_3に変更

--- scripts/Validationデータの変更_wandb/ring_augmentation.py
+++ scripts/Validationデータの変更_wandb/ring_augmentation.py
@@ -222,7 +222,7 @@ def catalogue(choice, args):
             print("#######################")
             print("   Ring selection")
             print("#######################")
-            rank_2_3 = np.load("rank_3.npy")
+            rank_2_3 = np.load("rank_2_3.npy")
             CH = CH.loc[rank_2_3]
         return CH
 
