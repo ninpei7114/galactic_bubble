0839411004c0772e5da9a027e172025a84c03bd0
shimpei <ninpei7114@gmail.com>
2023-09-20 11:58:40 +0900

    不要箇所の削除

--- scripts/Validationデータの変更_wandb/make_data.py
+++ scripts/Validationデータの変更_wandb/make_data.py
@@ -68,25 +68,8 @@ class make_training_val_data:
             ],
         )
 
-        # else:
-        #     ## 'spitzer_29400+0000_rgb'は、8µmのデータが全然ないため使用しない
-        #     # fmt: off
-        #     train_l = [
-        #         "spitzer_02100+0000_rgb", "spitzer_04200+0000_rgb", "spitzer_33300+0000_rgb", "spitzer_35400+0000_rgb",
-        #         "spitzer_00300+0000_rgb", "spitzer_02400+0000_rgb", "spitzer_04500+0000_rgb", "spitzer_31500+0000_rgb",
-        #         "spitzer_33600+0000_rgb", "spitzer_35700+0000_rgb", "spitzer_00600+0000_rgb", "spitzer_02700+0000_rgb",
-        #         "spitzer_04800+0000_rgb", "spitzer_29700+0000_rgb", "spitzer_31800+0000_rgb", "spitzer_03000+0000_rgb",
-        #         "spitzer_05100+0000_rgb", "spitzer_30000+0000_rgb", "spitzer_32100+0000_rgb", "spitzer_01200+0000_rgb",
-        #         "spitzer_03300+0000_rgb", "spitzer_05400+0000_rgb", "spitzer_30300+0000_rgb", "spitzer_32400+0000_rgb",
-        #         "spitzer_34500+0000_rgb", "spitzer_01500+0000_rgb", "spitzer_03600+0000_rgb", "spitzer_05700+0000_rgb",
-        #         "spitzer_30600+0000_rgb", "spitzer_32700+0000_rgb", "spitzer_34800+0000_rgb", "spitzer_01800+0000_rgb",
-        #         "spitzer_06000+0000_rgb", "spitzer_30900+0000_rgb", "spitzer_33000+0000_rgb", "spitzer_35100+0000_rgb"
-        #         ]
-        #     # fmt: on
-        #     self.train_l = sorted(train_l)
-
         ## 必要なフォルダの作成
-        self.save_data_path = args.savedir_path + "".join("dataset") + "/" + augmentation_name.split("/")[-1]
+        self.save_data_path = args.savedir_path + "/" + "".join("dataset") + "/" + augmentation_name.split("/")[-1]
         os.makedirs(self.save_data_path, exist_ok=True)
         os.makedirs(self.args.savedir_path + "".join("dataset"), exist_ok=True)
 
@@ -130,17 +113,6 @@ class make_training_val_data:
             for i, k in enumerate(NonRing_path):
                 shutil.copyfile(k, f"{self.save_data_path}/train/nonring/class{cl}/NonRing_{i}.png")
                 shutil.copyfile(k[:-3] + "json", f"{self.save_data_path}/train/nonring/class{cl}/NonRing_{i}.json")
-        # else:
-        #     ## デフォルトのNonRingをcopyする。
-        #     NonRing_origin = glob.glob("/workspace/NonRing_png/default_NonRing_png/train/*.png")
-        #     Choice_NonRing = self.Data_rg.choice(
-        #         NonRing_origin, int(self.train_data.shape[0]) * self.args.NonRing_ratio, replace=False
-        #     )
-        #     for i in Choice_NonRing:
-        #         shutil.copyfile(i, "%s/train/nonring/%s" % (self.save_data_path, i.split("/")[-1]))
-        #         shutil.copyfile(
-        #             i[:-3] + "json", "%s/train/nonring/%s" % (self.save_data_path, i.split("/")[-1][:-3] + "json")
-        #         )
 
         for cl in NonRing_class_num:
             with tarfile.open(f"{self.save_data_path}/bubble_dataset_train_nonring_class{cl}.tar", "w:gz") as tar:
@@ -154,9 +126,6 @@ class make_training_val_data:
         Params:
             train_cfg (_type_): _description_
         """
-
-        ## ********* 各領域ごとに *********
-        # if self.args.region_suffle:
         os.makedirs(f"{self.save_data_path}/val", exist_ok=True)
         print("MAKE VALIDATION DATA ...")
 
@@ -170,23 +139,6 @@ class make_training_val_data:
         for k in Val_origin:
             shutil.copyfile(k, f"{self.save_data_path}/val/{k.split('/')[-1][:-4]}.png")
             shutil.copyfile(k[:-3] + "json", f"{self.save_data_path}/val/{k.split('/')[-1][:-4]}.json")
-
-        ## ********* デフォルト領域で *********
-        # else:
-        #     ## Validationデータをcopyする。
-        #     ## Ringデータのコピー
-        #     shutil.copytree("/workspace/val_png/default_val", f"{self.save_data_path}/val", dirs_exist_ok=True)
-
-        #     ## Non-Ringデータのコピー
-        #     Val_default_path = glob.glob("/workspace/NonRing_png/default_NonRing_png/val/*.png")
-        #     Choice_NonRing = self.Data_rg.choice(
-        #         Val_default_path,
-        #         int(len(glob.glob(f"{self.save_data_path}/val/*"))) * self.args.NonRing_ratio,
-        #         replace=False,
-        #     )
-        #     for i, k in enumerate(Choice_NonRing):
-        #         shutil.copyfile(k, f"{self.save_data_path}/val/NonRing_{i}.png")
-        #         shutil.copyfile(k[:-3] + "json", f"{self.save_data_path}/val/NonRing_{i}.json")
 
         ## tarファイルに変換
         with tarfile.open(f"{self.save_data_path}/bubble_dataset_val.tar", "w:gz") as tar:
@@ -208,8 +160,6 @@ class make_training_val_data:
         print_and_log(
             self.f_log,
             [
-                # f"Ring NonRing ratio = 1 : {self.args.NonRing_ratio}",
-                # " ",
                 "Ring & Non-Ring num",
                 f">>> Train Ring num: {train_Ring_num}",
                 f">>> Val Ring num: {val_Ring_num}",
