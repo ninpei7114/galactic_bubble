0553f2b36ffeceb670e1bbc137c16403a06b8916
shimpei <ninpei7114@gmail.com>
2023-10-16 07:31:06 +0900

    ChuchwellとMWPを合算させたカタログを追加

--- scripts/wandb_training/ring_augmentation.py
+++ scripts/wandb_training/ring_augmentation.py
@@ -240,5 +240,34 @@ def catalogue(choice, ring_select=False, rank_path="rank_3.npy"):
             MWP = MWP.loc[rank_3]
         return MWP
 
+    elif choice == "SUM":
+        viz = astroquery.vizier.Vizier(columns=["*"])
+        viz.ROW_LIMIT = -1
+        bub_2006 = viz.query_constraints(catalog="J/ApJ/649/759/bubbles")[0].to_pandas()
+        bub_2007 = viz.query_constraints(catalog="J/ApJ/670/428/bubble")[0].to_pandas()
+        bub_2006_change = bub_2006.set_index("__CPA2006_")
+        bub_2007_change = bub_2007.set_index("__CWP2007_")
+        CH = pd.concat([bub_2006_change, bub_2007_change])
+        CH["CH"] = CH.index
+
+        viz = astroquery.vizier.Vizier(columns=["*"])
+        viz.ROW_LIMIT = -1
+        MWP = viz.query_constraints(catalog="2019yCat..74881141J ")[0].to_pandas()
+        MWP.loc[MWP["GLON"] >= 358.446500015535, "GLON"] -= 360
+        MWP.index = MWP["MWP"].tolist()
+        MWP = MWP.rename({"MajAxis": "Rout"}, axis="columns")
+        CH_MWP = pd.concat([CH, MWP])
+
+        if ring_select:
+            print("\n#######################")
+            print("   Ring selection")
+            print("#######################")
+            MWP_name = np.load("MWP_rank3_name.npy")
+            CH_name = np.load("rank_2_3.npy")
+            CH_MWP_name = np.concatenate([CH_name, MWP_name])
+            CH_MWP.loc[CH_MWP_name]
+
+        return CH_MWP
+
     else:
         print("this choice catalogue does not exist")
