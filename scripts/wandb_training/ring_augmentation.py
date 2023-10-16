8459bb89ac3284d2f110482896a10b2621a5adc1
shimpei <ninpei7114@gmail.com>
2023-10-16 18:10:43 +0900

    SUMで使用するリングを参照するファイルを変更

--- scripts/wandb_training/ring_augmentation.py
+++ scripts/wandb_training/ring_augmentation.py
@@ -264,7 +264,7 @@ def catalogue(choice, ring_select=False, rank_path="rank_3.npy"):
             print("   Ring selection")
             print("#######################")
             MWP_name = np.load("MWP_rank3_name.npy")
-            CH_name = np.load("rank_2_3.npy")
+            CH_name = np.load("rank_3.npy")
             CH_MWP_name = np.concatenate([CH_name, MWP_name])
             CH_MWP.loc[CH_MWP_name]
 
