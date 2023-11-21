b984e7b608af54e0ba5bdb1c0267a0acc801269a
shimpei <ninpei7114@gmail.com>
2023-11-21 13:24:43 +0900

    モデルのパラメータを読み込むpathを変更

--- scripts/wandb_training/LMC_Cygnus_GP_infer/LMC_Cygnus_GP_infer.py
+++ scripts/wandb_training/LMC_Cygnus_GP_infer/LMC_Cygnus_GP_infer.py
@@ -54,7 +54,9 @@ def main(args):
     artifact = api.artifact(f"{args.model_ver}")
     artifact.download(f"{args.model_download_dir}" + "/artifacts/" + args.model_ver.split("/")[-1])
     net_w = SSD()
-    net_weights = torch.load(args.model_download_dir + "/artifacts/" + args.model_ver.split("/")[-1])
+    net_weights = torch.load(
+        args.model_download_dir + "/artifacts/" + args.model_ver.split("/")[-1] + "/earlystopping.pth"
+    )
     net_w.load_state_dict(net_weights["model_state_dict"])
     del net_weights
     net_w.to(device)
