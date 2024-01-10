73f53e41f0bd439371764be3be7767c066b6fc45
shimpei <ninpei7114@gmail.com>
2024-01-10 13:12:36 +0900

    all_iterをintで包む

--- scripts/photoutils/LMC_Cygnus_GP_infer/LMC_Cygnus_GP_infer.py
+++ scripts/photoutils/LMC_Cygnus_GP_infer/LMC_Cygnus_GP_infer.py
@@ -68,7 +68,7 @@ def main(args):
             num_workers=2,
             pin_memory=True,
         )
-        all_iter = len(glob.glob(f"{args.Cygnus_LMC_png}/{region}/*.png")) / 128
+        all_iter = int(len(glob.glob(f"{args.Cygnus_LMC_png}/{region}/*.png")) / 128)
         iteration = 0
         ################
         ## INFER PART ##
