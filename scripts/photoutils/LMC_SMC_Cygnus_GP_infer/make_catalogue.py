ba02db2fede0961d86ec955977df559ec23e857b
ninpei7114 <ninpei7114@gmail.com>
2024-03-06 12:35:34 +0900

    Cygnus解析時にaplpyで参照するファイルを変更

--- scripts/photoutils/LMC_SMC_Cygnus_GP_infer/make_catalogue.py
+++ scripts/photoutils/LMC_SMC_Cygnus_GP_infer/make_catalogue.py
@@ -61,7 +61,7 @@ def main(args):
         elif region == "Cygnus":
             r_fits_path = args.Cygnus_data_path + "/M1_fits_file/M1_cygnus_2.4.fits"
             g_fits_path = args.Cygnus_data_path + "/I4_fits_file/I4_2.4_reg.fits"
-            save_png_name = f"{args.Cygnus_data_path}/RG_30.0_100.0_10.0_150.0_30.0_100.0.png"
+            save_png_name = f"{args.Cygnus_data_path}/RG_30.0_80.0_30.0_100.0_100000.0_100000.0.png"
             fits_path = [[r_fits_path, g_fits_path, region, save_png_name]]
 
         for fp in fits_path:
